fac89ddcb8f8b03c0c68ac88c448bd75b85d4297
Kacper <kandahari1461@gmail.com>
2023-05-08 11:07:36 +0200

    fixed typo

--- src/bump_node.cpp
+++ src/bump_node.cpp
@@ -3,25 +3,21 @@
 #include <std_msgs/Float32.h>
 
 
-// This code makes the Leo Rover lift one of it's wheels. 
-// The one on the front. On the right. No other. It's hard coded.
-// Also - it may not work. You need some pretty grippy surface for this thing 
-// to work as intended. No additional weight on the rover might help. Good luck. 
+// This code makes the Leo Rover lift one of its wheels - the one on the front, on the right. No other. It's hard-coded.
+// Also, it may not work. You need a pretty grippy surface for this thing to work as intended. 
+// No additional weight on the rover might help. Good luck.
 
-
-// Send this message when the rover (and this node) is running to lift the wheel:
+// Send this message when the rover (and this node) are running to lift the wheel:
 // rostopic pub cmd_bump std_msgs/Time "data:
-//   secs: 0
-//   nsecs: 750000000" 
+// secs: 0
+// nsecs: 750000000"
 
+// Send a similar one, with a random time, to lower the wheel.
 
-// Send similar one, with random time, to lover the wheel. 
-
-// Using:
+// You can make the wheel bounce up and down using:
 // rostopic pub -r 1 cmd_bump std_msgs/Time "data:
-//   secs: 0
-//   nsecs: 750000000" 
-// You can make the wheel bounce up and down 
+// secs: 0
+// nsecs: 750000000"
 
 
 ros::Subscriber sub;
